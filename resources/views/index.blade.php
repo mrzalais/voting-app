91f05e9d8fd62661b1b50cfd51db71e9d70b1b9a
mrzalais <lejnieksedijs@gmail.com>
2021-10-31 21:15:05 +0200

    Episode 3 - Design Idea Cards

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -29,4 +29,76 @@
             </div>
         </div>
     </div> <!-- end filters -->
+
+    <div class="ideas-container space-y-6 my-6">
+        <div class="idea-container hover:shadow-card transition duration-150 ease-in bg-white 
+        rounded-xl flex cursor-pointer">
+            <div class="border-r border-gray-100 px-5 py-8">
+                <div class="text-center">
+                    <div class="font-semibold text-2xl">12</div>
+                    <div class="text-gray-500">Votes</div>
+                </div>
+
+                <div class="mt-8">
+                    <button class="w-20 bg-gray-200 border border-gray-200 
+                    hover:border-gray-400 font-bold text-xxs uppercase rounded-xl transition 
+                    duration-150 ease-in px-4 py-3">Vote</button>
+                </div>
+            </div>
+            <div class="flex px-2 py-6">
+                <a href="#" class="flex-none">
+                    <img src="https://source.unsplash.com/200x200/?face&crop=face&v=1" alt="avatar" 
+                    class="w-14 h-14 rounded-xl">
+                </a>
+                <div class="mx-4">
+                    <h4 class="text-xl font-semibold">
+                        <a href="#" class="hover:underline">A random title can go here</a>
+                    </h4>
+                    <div class="text-gray-600 mt-3 line-clamp-3 transition duration-150">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur fugiat
+                         in rerum tempore natus sint cum sapiente. Facere aliquid illo ducimus optio
+                          aperiam neque, dolorem iste! Perspiciatis officiis sunt molestias est 
+                          similique? Fuga aspernatur delectus facilis ipsa dignissimos, et numquam 
+                          cupiditate animi laboriosam iusto perspiciatis! Delectus repellendus 
+                          oloribus commodi nesciunt officiis neque aliquam! Vero eos tenetur dolor 
+                          deserunt ipsam quidem! Voluptatem fugiat maiores tempore, deserunt ea 
+                          cupiditate ex fugit! Officiis, minima! Tenetur sunt voluptatibus quas 
+                          expedita vel consectetur delectus, fugit eius autem sint minima vitae, 
+                          pariatur vero, obcaecati eum placeat optio officiis? Aut officiis 
+                          voluptatem deserunt excepturi, vel odit magnam.
+                    </div>
+                    <div class="flex items-center justify-between mt-6">
+                        <div class="flex items-center text-xs text-gray-400 font-semibold 
+                        space-x-2">
+                            <div>10 hours ago</div>
+                            <div>&bull;</div>
+                            <div>Category 1</div>
+                            <div>&bull;</div>
+                            <div class="text-gray-900">3 Comments</div>
+                        </div>
+                        <div class="flex items-center space-x-2">
+                            <div class="bg-gray-200 text-xxs font-bold uppercase leading-none 
+                            rounded-full text-center w-28 h-7 py-2 px-4">Open</div>
+                            <button class="relative bg-gray-100 hover:bg-gray-200 rounded-full 
+                            h-7 transition duration-150 ease-in px-3"> 
+                            <svg fill="currentColor" width="24" height="6"><path d="M2.97.061A2.969 
+                                2.969 0 000 3.031 2.968 2.968 0 002.97 6a2.97 2.97 0 100-5.94zm9.184
+                                 0a2.97 2.97 0 100 5.939 2.97 2.97 0 100-5.939zm8.877 0a2.97 2.97 0 
+                                 10-.003 5.94A2.97 2.97 0 0021.03.06z" 
+                                 style="color: rgba(163, 163, 163, .5)">
+                            </svg> 
+                            <ul class="absolute w-44 text-left font-semibold bg-white 
+                            shadow-dialog rounded-xl py-3">
+                                <li><a href="#" class="hover:bg-gray-100 block transition 
+                                    duration-150 ease-in px-5 py-3">Mark as Spam</a></li>
+                                <li><a href="#" class="hover:bg-gray-100 block transition 
+                                    duration-150 ease-in px-5 py-3">Delete Post</a></li>
+                            </ul>
+                        </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div><!-- end of idea-container -->
+    </div><!-- end of ideas container -->
 </x-app-layout>
