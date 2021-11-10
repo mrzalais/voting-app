9b42c74366143e886b381d603d9c13cda383001f
mrzalais <lejnieksedijs@gmail.com>
2021-11-11 00:11:25 +0200

    Add a couple more tests

--- tests/Feature/GravatarTest.php
+++ tests/Feature/GravatarTest.php
@@ -31,7 +31,7 @@ class GravatarTest extends TestCase
         );
 
         $response = Http::get($user->getAvatar());
-        
+
         $this->assertTrue($response->successful());
     }
 
@@ -53,5 +53,45 @@ class GravatarTest extends TestCase
                 . '.png',
             $gravatarUrl
         );
+    }
+
+    /** @test */
+    public function user_can_generate_gravatar_defualt_image_when_no_email_found_first_character_0()
+    {
+        $user = User::factory()->create([
+            'name' => 'Andre',
+            'email' => '0fakeemail@fakeemail.com',
+        ]);
+
+        $gravatarUrl = $user->getAvatar();
+
+        $this->assertEquals(
+            'https://www.gravatar.com/avatar/' . md5($user->email) . '?s=200&d=https://s3.amazonaws.com/laracasts/images/forum/avatars/default-avatar-27.png',
+            $gravatarUrl
+        );
+
+        $response = Http::get($user->getAvatar());
+
+        $this->assertTrue($response->successful());
+    }
+
+    /** @test */
+    public function user_can_generate_gravatar_defualt_image_when_no_email_found_first_character_9()
+    {
+        $user = User::factory()->create([
+            'name' => 'Andre',
+            'email' => '9fakeemail@fakeemail.com',
+        ]);
+
+        $gravatarUrl = $user->getAvatar();
+
+        $this->assertEquals(
+            'https://www.gravatar.com/avatar/' . md5($user->email) . '?s=200&d=https://s3.amazonaws.com/laracasts/images/forum/avatars/default-avatar-36.png',
+            $gravatarUrl
+        );
+
+        $response = Http::get($user->getAvatar());
+
+        $this->assertTrue($response->successful());
     }
 }
