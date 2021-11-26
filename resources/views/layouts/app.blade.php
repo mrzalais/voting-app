6b4cf3dd07339b57e6deebc8d68a26735e18f758
mrzalais <lejnieksedijs@gmail.com>
2021-11-26 23:54:25 +0200

    Episode 24 - Status Filters

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,19 +30,20 @@
                             @csrf
 
                             <a :href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                    this.closest('form').submit();">
+                                                                            this.closest('form').submit();">
                                 {{ __('Log Out') }}
                             </a>
                         </form>
                     @else
                         <a href="{{ route('login') }}"
                             class="text-sm 
-                        text-gray-700 dark:text-gray-500 underline">Log in</a>
+                            text-gray-700 dark:text-gray-500 underline">Log
+                            in</a>
 
                         @if (Route::has('register'))
                             <a href="{{ route('register') }}"
                                 class="ml-4 text-sm 
-                            text-gray-700 dark:text-gray-500 underline">Register</a>
+                                text-gray-700 dark:text-gray-500 underline">Register</a>
                         @endif
                     @endauth
                 </div>
@@ -56,14 +57,14 @@
     <main class="container mx-auto max-w-custom flex flex-col md:flex-row">
         <div class="w-70 mx-auto md:mx-0 md:mr-5">
             <div class="bg-white md:sticky md:top-8 border-2 border-blue rounded-xl mt-16" style="
-                        border-image-source: linear-gradient(to bottom, rgba(50, 138, 241, 0.22), 
-                        rgba(99, 123, 255, 0));
-                        border-image-slice: 1;
-                        background-image: linear-gradient(to bottom, #ffffff, #ffffff), 
-                        linear-gradient(to bottom, rgba(50, 138, 241, 0.22), 
-                        rgba(99, 123, 255, 0));
-                        background-origin: border-box;
-                        background-clip: content-box, border-box;">
+                            border-image-source: linear-gradient(to bottom, rgba(50, 138, 241, 0.22), 
+                            rgba(99, 123, 255, 0));
+                            border-image-slice: 1;
+                            background-image: linear-gradient(to bottom, #ffffff, #ffffff), 
+                            linear-gradient(to bottom, rgba(50, 138, 241, 0.22), 
+                            rgba(99, 123, 255, 0));
+                            background-origin: border-box;
+                            background-clip: content-box, border-box;">
                 <div class="text-center px-6 py-2 pt-6">
                     <h3 class="font-semibold text-base">Add an idea</h3>
                     <p class="text-xs mt-4">
@@ -79,55 +80,26 @@
                     <livewire:create-idea />
                 @else
                     <div class="my-6 text-center">
-                        <a 
-                        href="{{ route('login') }}"
-                        type="submit"
-                        class="inline-block justify-center w-1/2
-                        h-11 text-xs bg-blue font-semibold rounded-xl border border-blue
-                        text-white hover:bg-blue-hover transition duration-150
-                        ease-in px-6 py-3">
+                        <a href="{{ route('login') }}" type="submit"
+                            class="inline-block justify-center w-1/2
+                            h-11 text-xs bg-blue font-semibold rounded-xl border border-blue
+                            text-white hover:bg-blue-hover transition duration-150
+                            ease-in px-6 py-3">
                             Login
                         </a>
-                        <a 
-                        href="{{ route('register') }}"
-                        class="inline-block justify-center w-1/2
-                        h-11 text-xs bg-gray-200 font-semibold rounded-xl border
-                        border-gray-200 hover:border-gray-400 transition duration-150
-                        ease-in px-6 py-3 mt-4">
+                        <a href="{{ route('register') }}"
+                            class="inline-block justify-center w-1/2
+                            h-11 text-xs bg-gray-200 font-semibold rounded-xl border
+                            border-gray-200 hover:border-gray-400 transition duration-150
+                            ease-in px-6 py-3 mt-4">
                             Sign Up
                         </a>
                     </div>
-                @endauth    
-
-                
+                @endauth
             </div>
         </div>
         <div class="w-full px-2 md:px-0 md:w-175">
-            <nav class="hidden md:flex items-center justify-between text-xs">
-                <ul class="flex uppercase font-semibold border-b-4 pb-3 space-x-10">
-                    <li><a href="#" class="border-b-4 pb-3 border-blue">All Ideas (87)</a></li>
-                    <li><a href="#"
-                            class="text-gray-400 transition duration-150 ease-in
-                             border-b-4 pb-3 hover:border-blue">Considering
-                            (6)</a></li>
-                    <li><a href="#"
-                            class="text-gray-400 transition duration-150 ease-in
-                                border-b-4 pb-3 hover:border-blue">In
-                            progress (1)</a></li>
-                </ul>
-
-                <ul class="flex uppercase font-semibold border-b-4 pb-3 space-x-10">
-                    <li><a href="#"
-                            class="text-gray-400 transition duration-150 ease-in
-                             border-b-4 pb-3 hover:border-blue">Implemented
-                            (10)</a></li>
-                    <li><a href="#"
-                            class="text-gray-400 transition duration-150 ease-in
-                                border-b-4 pb-3 hover:border-blue">Closed
-                            (55)</a></li>
-                </ul>
-            </nav>
-
+            <livewire:status-filters />
             <div class="mt-8">
                 {{ $slot }}
             </div>
