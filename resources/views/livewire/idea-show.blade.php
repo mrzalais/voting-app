9843eaddca17bb03e22af871e6c16e1eca1ed9d2
mrzalais <lejnieksedijs@gmail.com>
2021-12-05 19:13:39 +0200

    Episode 38 - Edit Idea Modal Transitions

--- resources/views/livewire/idea-show.blade.php
+++ resources/views/livewire/idea-show.blade.php
@@ -48,7 +48,10 @@
                                 class="absolute w-44 text-left font-semibold bg-white 
                             shadow-dialog rounded-xl z-10 py-3 md:ml-8 top-8 md:top-6 right-0 
                             md:left-0">
-                                <li><a href="#"
+                                <li><a href="#" @click="
+                                    isOpen = false 
+                                    $dispatch('custom-show-edit-modal')
+                                    "
                                         class="hover:bg-gray-100 block transition 
                             duration-150 ease-in px-5 py-3">Edit
                                         Idea</a></li>
