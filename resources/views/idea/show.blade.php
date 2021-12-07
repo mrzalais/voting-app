13c3ba781a9c14200176c5d8314ac3cabca5b5cd
mrzalais <lejnieksedijs@gmail.com>
2021-12-07 23:06:51 +0200

    Episode 45 - Refactor Modals to Blade Component

--- resources/views/idea/show.blade.php
+++ resources/views/idea/show.blade.php
@@ -10,21 +10,7 @@
 
     <livewire:idea-show :idea="$idea" :voteCount="$voteCount" />
 
-    @can('update', $idea)
-        <livewire:edit-idea :idea="$idea" />
-    @endcan
-
-    @can('delete', $idea)
-        <livewire:delete-idea :idea="$idea" />
-    @endcan
-
-    @auth
-        <livewire:mark-idea-as-spam :idea="$idea" />
-    @endauth
-
-    @admin
-    <livewire:mark-idea-as-not-spam :idea="$idea" />
-    @endadmin
+    <x-modals-container :idea="$idea" />
 
     </div><!-- end ideas and button container -->
 
