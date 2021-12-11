42525f4b23ff3b7d28bd7cac53f76ba27e55c4ca
mrzalais <lejnieksedijs@gmail.com>
2021-12-11 13:13:29 +0200

    Episode 46 - Success Message Notification CSS

--- resources/views/components/modal-confirm.blade.php
+++ resources/views/components/modal-confirm.blade.php
@@ -13,7 +13,7 @@
     $nextTick(() => $refs.confirmButton.focus())
     " x-init="window.livewire.on('{{ $eventToCloseModal }}', () => {
     isOpen = false
-})" class="fixed z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+})" class="fixed z-20 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
     <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
         <div x-show.transition.opacity="isOpen" class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
             aria-hidden="true"></div>
